c1ba81818d5333deddbf52f48cfb0c6454981df3
aiescola <basch92@gmail.com>
2020-06-08 17:31:24 +0200

    add aliases, .config and improve scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,4 @@
 #!bin/sh
-go_version="go1.14.4.linux-amd64.tar.gz"
 
 sudo apt update 1>/dev/null
 sudo apt install snapd -y
@@ -12,15 +11,14 @@ sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ 1>
 sudo apt-get install -y software-properties-common 1>/dev/null
 sudo apt-get install -y adoptopenjdk-8-openj9 
 
-# GO
-wget https://dl.google.com/go/$go_version
-sudo tar -C /usr/local -xzf $go_version 1>/dev/null
-
 # Visual Studio Code
-snap install --classic code
+snap install code --classic
 
 # Android Studio
 snap install android-studio --classic 
+
+# Go
+snap install go --classic
 
 # Zsh
 sudo apt install zsh -y
