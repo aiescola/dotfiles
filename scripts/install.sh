7e2788e8cc02e7a67ff6e868ad5ebf8500ccab19
Aiescola <basch92@gmail.com>
2020-06-27 17:37:21 +0200

    add links, git config and update .zshrc

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,13 +3,14 @@
 sudo apt update 1>/dev/null
 sudo apt install snapd -y
 
-sudo apt install git -y
-
 # JAVA
 wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
 sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ 1>/dev/null
 sudo apt-get install -y software-properties-common 1>/dev/null
 sudo apt-get install -y adoptopenjdk-8-openj9 
+
+# Terminal radio
+sudo apt-get install mpg123
 
 # Visual Studio Code
 snap install code --classic
@@ -23,3 +24,5 @@ snap install go --classic
 # Zsh
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+chsh -s $(which zsh)
